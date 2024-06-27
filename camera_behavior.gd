bdb2256de50b52ef978767a5c19f40fba08b8763
majg8a <majg8a@gmail.com>
2024-06-26 21:13:34 -0400

    feat: i didn't like mouse camera rotation

--- camera_behavior.gd
+++ camera_behavior.gd
@@ -1,3 +1,13 @@
 extends Marker3D
+@onready var player = get_node("../player")
 func _ready():
-	print("hello")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _physics_process(delta):
+	position = player.position
+
+#func _input(e):
+#	if e is InputEventMouseMotion:
+#		var mousePosition = e.relative
+#		rotate_object_local(Vector3.DOWN, mousePosition.x  * .001)
+#		rotate_object_local(Vector3.LEFT, mousePosition.y  * .001)
